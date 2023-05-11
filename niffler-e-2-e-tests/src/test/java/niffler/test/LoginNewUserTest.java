4fe3d5a6c15b4910cc690ee7c4e71fb278c1e511
human122 <no_email@example.com>
2023-05-11 20:26:49 +0300

    Видео урок 3: JDBC

---
+++ niffler-e-2-e-tests/src/test/java/niffler/test/LoginNewUserTest.java
@@ -0,0 +1,57 @@
+package niffler.test;
+
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Allure;
+import niffler.db.dao.NifflerUsersDAO;
+import niffler.db.dao.NifflerUsersDAOJdbc;
+import niffler.db.entity.Authority;
+import niffler.db.entity.AuthorityEntity;
+import niffler.db.entity.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginNewUserTest extends BaseWebTest {
+
+  private NifflerUsersDAO usersDAO = new NifflerUsersDAOJdbc();
+  private UserEntity ue;
+
+  @BeforeEach
+  void createUserForTest() {
+    ue = new UserEntity();
+    ue.setUsername("valentin");
+    ue.setPassword("12345");
+    ue.setEnabled(true);
+    ue.setAccountNonExpired(true);
+    ue.setAccountNonLocked(true);
+    ue.setCredentialsNonExpired(true);
+
+    ue.setAuthorities(Arrays.stream(Authority.values()).map(
+            a -> {
+              AuthorityEntity ae = new AuthorityEntity();
+              ae.setAuthority(a);
+              return ae;
+            }
+    ).toList());
+
+    usersDAO.createUser(ue);
+  }
+
+  @Test
+  void loginTest() throws IOException {
+    Allure.step("open page", () -> Selenide.open("http://127.0.0.1:3000/main"));
+    $("a[href*='redirect']").click();
+    $("input[name='username']").setValue(ue.getUsername());
+    $("input[name='password']").setValue(ue.getPassword());
+    $("button[type='submit']").click();
+
+    $("a[href*='friends']").click();
+    $(".header").should(visible).shouldHave(text("Niffler. The coin keeper."));
+  }
+}
